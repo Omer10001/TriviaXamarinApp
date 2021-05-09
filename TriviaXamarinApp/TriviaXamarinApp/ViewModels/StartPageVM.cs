89019a312d970a323e372a465ad50b0ddd7449f8
User <User@LAB2-13>
2021-05-09 16:00:14 +0300

    changes

---
+++ TriviaXamarinApp/TriviaXamarinApp/ViewModels/StartPageVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using TriviaXamarinApp.Models;
+using TriviaXamarinApp.Services;
+
+
+namespace TriviaXamarinApp.ViewModels
+{
+    class StartPageVM
+    {
+        icomm
+    }
+}
